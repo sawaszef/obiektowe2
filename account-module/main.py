9e46fe411ebe47139d2cd6b87bcfb48a9ea6a25d
sawaszef <80282140+sawaszef@users.noreply.github.com>
2022-03-06 23:28:47 +0100

    Update main.py

--- account-module/main.py
+++ account-module/main.py
@@ -2,15 +2,23 @@ import account as a
 
 if __name__ == '__main__':
 
-    print("Tworze konto z saldem 50.")
-    account1 = a.Account(50)
+    balance = float(input("Podaj ilosc srodkow jakie chcesz poczatkowo miec na koncie.\n"))
+    account1 = a.Account(balance)
     print(account1)
-    print("Dodaje 25 srodkow do konta.")
-    account1.pay(25)
-    print(account1)
-    print("Zabieram 50 srodkow z konta.")
-    account1.take(50)
-    print(account1)
-    print("Zabieram 30 srodkow z konta, ale jako ze mam mniej niz chce zabrac, zostaje o tym poinformowany i transakcja jest anulowana.")
-    account1.take(30)
-    print(account1)
+
+    while True:
+        amount = float(input("Podaj ilosc srodkow ktora chcesz dac lub zabrac z konta.\n"))
+        choice = input("Chcesz dodac czy zabrac srodki z konta? (add/take). '0' Konczy program.\n")
+
+        if choice == 0:
+            break
+        elif choice not in ["add", "take"]:
+            print("Podaj poprawna komende.")
+            continue
+
+        if choice == "add":
+            account1.pay(amount)
+        elif choice == "take":
+            account1.take(amount)
+
+        print(account1)
